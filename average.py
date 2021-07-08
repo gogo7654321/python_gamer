da29ce0fea4314a411958060fa0dd7970fe615f5
gogo7654321 <npatel012010@gmail.com>
2021-07-08 19:25:52 -0400

    removing G to correc tthe code

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
 print("5 numbers and I will,give you the average. Use zeros to enter less than 5")
 a = int (input())
 b = int(input())
-c = int(input())g
+c = int(input())
 d = int(input())
 e = int(input())
 ans = (a + b + c + d + e)/5
