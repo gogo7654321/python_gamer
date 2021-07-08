4b6fc10d377e692210e4c848195d08f027a99d79
gogo7654321 <npatel012010@gmail.com>
2021-07-08 19:22:42 -0400

    trying g

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
 print("5 numbers and I will,give you the average. Use zeros to enter less than 5")
 a = int (input())
 b = int(input())
-c = int(input())
+c = int(input())g
 d = int(input())
 e = int(input())
 ans = (a + b + c + d + e)/5
