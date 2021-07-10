c3ae95a8d35efc6558518924c645f3653051d87d
gogo7654321 <npatel012010@gmail.com>
2021-07-10 13:12:38 -0400

    removing G to correc tthe code

---
+++ order low to great.py
@@ -0,0 +1,9 @@
+print ("enter 5 numbers and i will order them from least to greatest")
+a =int(input())
+b =int(input())
+c =int(input())
+d =int(input())
+e =int(input())
+numbers = [a,b,c,d,e]
+sorted(numbers)
+print(numbers)
